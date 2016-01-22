9c193fd19128dc3fab3cfa72189b1014f69bdd34
Jonathan Kilgore <jonathan.kilgore58@gmail.com>
2016-01-22 09:16:29 -0600

    Initial Commit

---
+++ TopCities/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopCities
+//
+//  Created by Jonathan Kilgore on 1/22/16.
+//  Copyright © 2016 Jonathan Kilgore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
